cd6e67b0a0007f337a3c219a0097abd06223f28d
Lumethys <lekhang8@gmail.com>
2025-08-22 00:42:07 +0700

    add Medieval font + impl carousel (additional styling) + impl detail page (in-progress)

--- app/pages/test.vue
+++ app/pages/test.vue
@@ -1,18 +1,24 @@
 <script setup lang="ts">
-const { data: items } = useFetchWantedPosts()
+const { data: items } = useFetchWantedPosts();
 </script>
 
 <template>
   <div class="flex flex-col items-center justify-center w-screen h-screen gap-2 bg-yellow-950">
     <UCarousel
-        v-slot="{ item }"
+        v-slot="{ item, index }"
         class="w-full max-w-5xl mx-auto"
         arrows
         :items="items"
         :ui="{ item: 'basis-1/3' }"
         loop
     >
-      <WantedPanel v-bind="item" />
+      <WantedPanel
+          v-bind="item"
+          :class="(index % 2 === 0)
+            ? 'bg-[url(/images/wanted-poster-background.png)]'
+            : 'bg-[url(/images/wanted-poster-background-2.png)]'
+          "
+      />
     </UCarousel>
   </div>
 </template>
